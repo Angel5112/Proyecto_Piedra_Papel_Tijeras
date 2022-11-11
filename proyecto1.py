04b02a635448e112853e806dc7e3afaea4b8c3d1
Angel Eduardo <aepatino.360@gmail.com>
2022-11-11 12:33:24 -0400

    Added the menu of the game.

--- proyecto1.py
+++ proyecto1.py
@@ -8,3 +8,18 @@ Fecha: 11/11/2022
 
 """
 
+# Task 1: Creacion del Menu de juego
+
+print("\n****** Piedra-Papel-Tijeras ******\n")
+print("Ingrese el modo de juego: \n")
+print("\t1 = Jugador vs. Computadora")
+print("\t2 = Jugador vs. Jugador\n")
+
+operacion = int(input("Modo de Juego: "))
+
+if (operacion == 1):
+    print("\n***** Jugador vs. Computadora *****\n")
+elif (operacion == 2):
+    print("\n***** Jugador vs. Jugador *****\n")
+else:
+    print("\nError: Numero ingresado no corresponde a operacion asignada! Saliendo del juego\n")
