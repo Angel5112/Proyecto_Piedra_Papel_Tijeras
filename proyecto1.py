9efbf521ef3e888067efaf7a43bd9e8162167860
Angel Eduardo <aepatino.360@gmail.com>
2022-11-11 12:19:14 -0400

    First commit. Added the basics of the project in the .py file.

---
+++ proyecto1.py
@@ -0,0 +1,10 @@
+"""
+
+Creacion de Proyecto: Piedra-Papel-Tijeras para utilizar todo lo aprendido sobre 
+tipos de datos, operadores y condicionales en Python
+
+Elaborado por: Angel Patino
+Fecha: 11/11/2022
+
+"""
+
