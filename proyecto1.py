74c04eba3241e25f3a8dcf6208304c94b8f9fe74
Angel Eduardo <aepatino.360@gmail.com>
2022-11-12 09:48:29 -0400

    Added the variables and basic loop of the game.

--- proyecto1.py
+++ proyecto1.py
@@ -11,15 +11,30 @@ Fecha: 11/11/2022
 # Task 1: Creacion del Menu de juego
 
 print("\n****** Piedra-Papel-Tijeras ******\n")
-print("Ingrese el modo de juego: \n")
-print("\t1 = Jugador vs. Computadora")
-print("\t2 = Jugador vs. Jugador\n")
 
-operacion = int(input("Modo de Juego: "))
+# Task 2: Creacion de variables para el juego
 
-if (operacion == 1):
-    print("\n***** Jugador vs. Computadora *****\n")
-elif (operacion == 2):
-    print("\n***** Jugador vs. Jugador *****\n")
-else:
-    print("\nError: Numero ingresado no corresponde a operacion asignada! Saliendo del juego\n")
+rounds = 0
+player1_victories = 0
+player2_victories = 0
+player1 = 0
+player2 = 0
+
+# Task 3: Creacion del ciclo base del juego
+
+while (rounds == 0):
+
+    print("Ingrese el modo de juego: \n")
+    print("\t1 = Jugador vs. Computadora")
+    print("\t2 = Jugador vs. Jugador\n")
+
+    operacion = int(input("Modo de Juego: "))
+
+    if (operacion == 1):
+        print("\n***** Jugador vs. Computadora *****\n")
+    elif (operacion == 2):
+        print("\n***** Jugador vs. Jugador *****\n")
+    else:
+        print("\nError: Numero ingresado no corresponde a operacion asignada! Saliendo del juego\n")
+
+    
